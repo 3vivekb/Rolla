04b714a4575648244469404c848e03ad0aed17fc
Vivek <3vivekb@gmail.com>
2015-01-11 00:16:07 -0800

    Startup fixed, and now it can build.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,14 @@ public class PlayerController : MonoBehaviour
 	//public float speed;
 	void Update ()
 	{
+		//Write an if statement here - if on a tablet use accel
+		// if on computer use getAxis.
 
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		//float moveHorizontal = Input.GetAxis ("Horizontal");
+		//float moveVertical = Input.GetAxis ("Vertical");
 
-		//float moveHorizontal = Input.acceleration.x;
-		//float moveVertical = Input.acceleration.y;
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = Input.acceleration.y;
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
---
+++ Assets/Startup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+//public static ScreenOrientation orientation;
+
+public class Startup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Start in landscape mode
+			Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
